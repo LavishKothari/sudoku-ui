ca4ef3017d02d1bb826881bb34adb17d237b3727
lavishkothari004@gmail.com <lavishkothari004@gmail.com>
2019-08-25 16:17:19 +0530

    Updates

---
+++ precache-manifest.55667dcbddec733006426bbcd2a59d36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "770ffaa3639affc7ac280e9cd5d70674",
+    "url": "/sudoku-ui/index.html"
+  },
+  {
+    "revision": "c64fe81a569f7304a7ce",
+    "url": "/sudoku-ui/static/css/main.d86d90b4.chunk.css"
+  },
+  {
+    "revision": "088932fa7f5db6a0e6e3",
+    "url": "/sudoku-ui/static/js/2.6481f9f8.chunk.js"
+  },
+  {
+    "revision": "c64fe81a569f7304a7ce",
+    "url": "/sudoku-ui/static/js/main.41e6337d.chunk.js"
+  },
+  {
+    "revision": "224500098d6ef324d5da",
+    "url": "/sudoku-ui/static/js/runtime~main.2f5382e6.js"
+  }
+]);
